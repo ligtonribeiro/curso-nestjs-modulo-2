17384d689e70b0c6134eefa06314ba5fef2e989d
Wellington Menezes <ligton.ribeiro@gmail.com>
2021-10-14 14:02:43 -0300

    Curso NestJS Alura

---
+++ src/usuario/usuario.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Post, Body }from '@nestjs/common'
+import { UsuarioService } from './usuario.service'
+
+@Controller('users')
+export class UsuarioController {
+    constructor(private usuarioService: UsuarioService) {}
+
+    @Post()
+    public cria(@Body() usuario) {
+        const usuarioCriado = this.usuarioService.cria(usuario)
+
+        return usuarioCriado
+    }
+}
